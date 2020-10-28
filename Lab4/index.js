6e48fc435d78e5037259a2a168397abd47247043
MihailSandulescu <sandulescu1mihail@gmail.com>
2020-10-28 18:45:38 +0200

    added Lab4

---
+++ Lab4/index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser'); 
+const jwt = require('jsonwebtoken');
+
+const config = {
+    secretKey: 'SuperSecretKey'
+}
+
+app.use(bodyParser.json());
+
+const authorizationMiddleware = (req, res, next) => {
+    const { authorization } = req.headers;
+    
+    if(!authorization) {
+        res.status(401)
+           .send({
+            status: 'not ok'
+        }) 
+    }
+
+    const jwtToken = authorization.replace('Bearer ', '');
+    
+    jwt.verify(jwtToken, config.secretKey, (err, decoded) => {
+        if(err) {
+            res.status(401)
+               .send({
+                status: 'not ok'
+            })
+        } else {
+            next();
+        }
+    })
+}
+
+app.post('/graphql', authorizationMiddleware, (req, res) => {
+    res.status(200)
+       .send({
+        status: 'ok'
+    });
+});
+
+app.post('/graphql/public', (req, res) => {
+    const { user, pass } = req.body;
+
+    if(user === "Gogu" && pass === "P@rOLA") {
+        jwt.sign({}, config.secretKey, (err, token) => {
+            res.send({
+                token
+            });
+        });
+    } else {
+        res.status(401)
+           .send({
+            status: 'not ok'
+        })
+    }
+});
+
+app.listen(port,() => {
+    console.log('server started');
+})
